4f4aab11791af6a7cab1a0afb8921bb864ecd0de
Victor G <victorgordian103@gmail.com>
2018-12-21 12:57:47 -0800

    Done

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 
 const express = require('express');
-const helmet = require('helmet')
+const helmet = require('helmet');
+const logger = require('morgan');
 
-const db2 = require('./data/helpers/userDb')
-const db = require('./data/helpers/postDb');
+const postDB = require('./data/helpers/userDb')
+const userDB = require('./data/helpers/postDb');
 const server = express();
 
 const PORT = 4000;
 
-
+server.use(express.json())
 server.use(helmet());
+server.use(logger('tiny'))
+
 
 server.get('/', (req, res) =>{
-    db.get()
+    userDB.get()
         .then((posts) =>{
             res.json(posts)
         })
@@ -22,16 +25,16 @@ server.get('/', (req, res) =>{
         })
 })
 
-server.get('/posts/:id', (req,res) =>{
+server.get('/user/:id', (req,res) =>{
     const { id } = req.params;
-    db2.getUserPosts(id)
+    postDB.getUserPosts(id)
         .then(post =>{
-            console.log(post.length)
+            
             if(post.length > 0){
                 res.json(post);
             } else{
                 res.status(404)
-                .json({message: "The post with the specified ID does not exist."})
+                .json({message: "The post(s) with the specified user ID does not exist."})
             }
         })
         .catch(err => {
@@ -41,6 +44,74 @@ server.get('/posts/:id', (req,res) =>{
         })
 })
 
+server.get('/post/:id', (req,res) =>{
+    const { id } = req.params;
+    
+    userDB.get(id)
+        .then(post =>{
+            
+            res.json(post);
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({message: "The post with the specified ID does not exist."})
+        })
+})
+
+
+server.post('/api/users', (req,res) => {
+    const user = req.body;
+    console.log(user)
+    if (user.name){
+        postDB.insert(user)
+    .then(info => {
+        postDB.get(info.id).then(response => {
+            res
+            .status(201)
+            .json(response)})
+        })
+        
+    .catch(err => {
+        res
+        .status(500)
+        .json({message: "failed to get user"})
+    })
+    }
+
+    else {
+        res
+        .status(400)
+        .json({message: "missing name"})
+    }
+    
+})
+
+server.post('/api/posts', (req,res) => {
+    const post = req.body;
+    if (post.text && post.userID){
+        userDB.insert(post)
+    .then(info => {
+        userDB.get(info).then(response => {
+            res
+            .status(201)
+            .json(response)})
+        })
+        
+    .catch(err => {
+        res
+        .status(500)
+        .json({message: "failed to get post"})
+    })
+    }
+
+    else {
+        res
+        .status(400)
+        .json({message: "missing text or user id"})
+    }
+    
+})
 
 server.listen(PORT, ()=> {
     console.log(`server is up and runing on port ${PORT}`)
